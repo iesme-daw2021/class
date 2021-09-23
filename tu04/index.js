a6d1ad3fc178e4a650cb2e14c7104c2b7bc59b52
Xavier Martinez <xavi12p@hotmail.com>
2021-09-23 20:09:55 +0200

    tu04

---
+++ tu04/index.js
@@ -0,0 +1,80 @@
+'use strict';
+// console.log('Hola');
+// console.log("Mundo");
+// const miNombre = 23;
+// console.log('Mi nombre es ' + miNombre + ' y vivo en Gandia');
+// console.log(`Mi nombre es ${miNombre} `);
+// const num1 = '23';
+// const num2 = 7;
+// const num3 = 'A';
+// const result = parseInt(num1) + num2;
+// const result2 = +num1 + num2;
+// const result3 = +num3 + num2;
+// console.log(result);
+// console.log(result2);
+// console.log(result3);
+
+// let undef;
+// const vnull = null;
+// const num1 = 23;
+// const num2 = 23n;
+
+// console.log(typeof calcularPeso);
+// console.log(typeof num2);
+
+// alert('Hola Mundo');
+// const result = prompt('Give me a number between 1 and 10', 1);
+// console.log(typeof result);
+// const xavi = 23n;
+// console.log(Boolean('  3A   '));
+// let numero = 2 ** 100;
+// console.log(numero);
+// console.log(typeof numero);
+// numero = 2 ** 53;
+// console.log(numero);
+// const result = confirm('Do you want exit');
+// console.log(result);
+// const result = 'Xavi';
+// console.log(result--);
+// console.log(result--);
+// console.log(result);
+// console.log(result > 'Xxxxxxxxxxxx');
+// if (2 > 3) {
+//   a = true;
+// } else {
+//   a = false;
+// }
+
+// const a = 2 > 3 ? true : false;
+// console.log(Boolean(NaN));
+// console.log('A' || 0);
+// const nickname = prompt('Give me your nickname');
+// const condicion = nickname || 'Without nickname';
+// // console.log(condicion);
+// const mayorEdad = true;
+// // if (!menorEdad) {
+// //   console.log('Eres adulto');
+// // }
+
+// mayorEdad && console.log('Eres adulto');
+
+// let x = 1;
+// x > 0 && console.log('mayor que cero');
+
+// let result2 = 5;
+// const valor2 = result2--;
+//  console.log(valor2);
+
+// result /= 5;
+// result = result / 5;
+
+// let x = '1';
+// console.log(typeof +x);
+
+// let x = 1;
+// console.log(typeof Boolean(x));
+
+let altura = 0;
+
+altura = altura ?? 100;
+console.log(altura);
