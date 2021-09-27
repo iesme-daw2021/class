56d5bbbce377ae36e2b0c8097d7e752918031e14
Xavi Martinez <xavi12p@hotmail.com>
2021-09-27 18:25:02 +0200

    examples return

--- tu04/tu05.js
+++ tu04/tu05.js
@@ -1,8 +1,18 @@
 function test() {
   console.log('test');
+  // return null;
+  // return NaN;
+  // return 1;
 }
 
 const result = test();
+console.log('result = ' + result);
+if (typeof result !== 'undefined') {
+  console.log('result ok; not undefined');
+}
+if (result !== undefined) {
+  console.log('result ok; not undefined');
+}
 if (result) {
-  console.log('pasa');
+  console.log('result ok; TRUE');
 }
