a03bb87d2b75900cc74a35fae9e140d56a4fb287
Xavier Martinez <xavi12p@hotmail.com>
2021-10-15 20:52:10 +0200

    tu08

---
+++ tu08/sample1.js
@@ -0,0 +1,16 @@
+const num = 1.2;
+// console.log(+num.toFixed(2) + 2);
+
+console.log(Math.round(2.1));
+console.log(Math.round(2.9));
+
+console.log(+(2.1).toFixed(2));
+console.log(+(2.9).toFixed(2));
+
+console.log('\u2614');
+
+let valor = 'Hello';
+valor.length = 3;
+console.log(valor.length);
+
+console.log('Hello'.charAt(0));
