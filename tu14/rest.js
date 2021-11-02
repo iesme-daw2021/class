c7d6d9a73a3ba28b8115f49614d00b36c841a8ae
Xavier Martinez <xavi12p@hotmail.com>
2021-11-02 17:58:05 +0100

    tu14

---
+++ tu14/rest.js
@@ -0,0 +1,3 @@
+function sumAll(a, b, ...elements) {}
+
+sumAll(1, 7, 3, 5, 10);
---
+++ tu14/spread.js
@@ -0,0 +1,10 @@
+const array = [3, 5, 1];
+
+// Solution 1
+const p1 = array[0];
+const p2 = array[1];
+const p3 = array[2];
+console.log(Math.max(p1, p2, p3));
+
+// Solution 2
+console.log(Math.max(...array));
