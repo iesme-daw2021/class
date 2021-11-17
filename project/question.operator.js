a501fed8f4292ffcabe8b3e2f1b96e60a4b15d1c
Xavier Martinez <xavi12p@hotmail.com>
2021-11-17 17:14:58 +0100

    project questions

--- project/question.operator.js
+++ project/question.operator.js
@@ -1,7 +1,8 @@
 const monster = {
   health: 10,
+  isDead: true,
 };
 
-const isDead = monster.health < 0 ? true : false;
+const isDead = monster.health < 0 ? true : monster.isDead ? true : false;
 
 console.log(isDead);
