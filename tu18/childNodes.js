49fbf30809c33e89674967831e0c51766a6809a4
Xavier Martinez <xavi12p@hotmail.com>
2021-11-25 18:24:15 +0100

    tu18

---
+++ tu18/childNodes.js
@@ -0,0 +1,12 @@
+// for (const node of document.body.childNodes) {
+//   console.log(node);
+// }
+
+const elemUl = document.querySelector('ul');
+console.log(elemUl.firstElementChild);
+
+// SI ES LIVE
+//const listNodes = elemUl.childNodes;
+
+// NO ES LIVE
+//document.querySelectorAll('li');
