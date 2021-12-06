84b25d672774b4e3d1f5fc77eb6e011b427da8f5
Xavi Martinez <xavi12p@hotmail.com>
2021-12-06 09:18:03 +0100

    tu24 - bubble

---
+++ tu24/bubble/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+  form.onclick = function (event) {
+    event.target.style.backgroundColor = 'yellow';
+
+    // chrome needs some time to paint yellow
+    setTimeout(() => {
+      alert('target = ' + event.target.tagName + ', this=' + this.tagName);
+      event.target.style.backgroundColor = '';
+    }, 0);
+  };
+});
